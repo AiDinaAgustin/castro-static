7faeb2a041a6792ec2206d48fd2ba235bafc96b0
AiDinaAgustin <aidinagustin2@gmail.com>
2023-10-30 12:34:20 +0700

    edit-dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js}" ,  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    colors: {
+      primary: '#262626',
+      secondary: '#4f4f4f',
+      three: '#1E2748',
+      four: '#111629',
+      five: '#17C3F9',
+      dark: '#212121',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      mono: ['Menlo', 'monospace'],
+      raleway: ['Montserrat', 'sans-serif'],
+      nunito: ['Playfair Display', 'serif'],
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
